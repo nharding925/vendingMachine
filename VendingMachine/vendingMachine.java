2fabe10112dab3f898629fc4ced0050d5e7ea67b
nharding925 <nharding925@gmail.com>
2026-02-12 19:20:10 -0500

    Added array with prices

--- VendingMachine/vendingMachine.java
+++ VendingMachine/vendingMachine.java
@@ -1,7 +1,14 @@
 package VendingMachine;
+import java.util.Scanner; 
 
 public class vendingMachine {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Array of snack names
+        String[] snacks = {"Doritos", "Snickers", "Oreos", "Coke", "Skittles"};
         
+        // Array of prices
+        double[] prices = {2.00, 1.00, 3.00, 2.00, 1.00};
     }
 }
